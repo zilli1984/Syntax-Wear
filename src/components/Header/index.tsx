d219d11630290014fde80c0c0635916a1d3d70ea
Fernanda Zilli <zilli_1984@hotmail.com>
2026-06-26 17:31:03 +0200

    feat: add shopping cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import Logo from "@/assets/images/logo.png";
 import IconUser from "@/assets/images/icon-user.png";
 import IconAbout from "@/assets/images/icon-about.png";
-import IconCart from "@/assets/images/icon-cart.png";
 import { Link } from "@tanstack/react-router";
+import { ShoppingCart } from "../ShoppingCart";
 
 export const Header = () => {
   return (
@@ -40,7 +40,7 @@ export const Header = () => {
                 <a href="#"><img src={IconAbout} alt="Ícone de sobre" /></a>
               </li>
               <li>
-                <a href="#"><img src={IconCart} alt="Ícone de carrinho" /></a>
+                <ShoppingCart />
               </li>
             </ul>
           </nav>
---
+++ src/components/ShoppingCart/index.tsx
@@ -0,0 +1,80 @@
+import Iconcart from "@/assets/images/icon-cart.png"
+import { useState } from "react"
+import MensTreeDasher from '@/assets/images/tree-dasher-2-natural-black-boyal-blue.webp'
+import MensTreeRunnerNz from '@/assets/images/tree-runner-nz-weathered-brown.webp'
+import MensWoolCruiser from '@/assets/images/wool-cruiser-burgundy.webp'
+import MensWoolCruiserSlipOn from '@/assets/images/wool-cruiser-slip-on-dark-grey.webp'
+import MensWoolCruiserWaterproof from '@/assets/images/wool-cruiser-waterproof-natural-black.webp'
+import { formatCurrency } from "../../utils/format-currency"
+
+const productsInCart = [{ id: 1, name: 'Produto 1', image: MensTreeDasher, price: 35, quantity: 5 },
+{ id: 2, name: 'Produto 2', image: MensTreeRunnerNz, price: 75, quantity: 2 },
+{ id: 3, name: 'Produto 3', image: MensWoolCruiser, price: 85, quantity: 4 },
+{ id: 4, name: 'Produto 4', image: MensWoolCruiserSlipOn, price: 135, quantity: 6 },
+{ id: 5, name: 'Produto 5', image: MensWoolCruiserWaterproof, price: 15, quantity: 2 },
+{ id: 2, name: 'Produto 2', image: MensTreeRunnerNz, price: 75, quantity: 2 },
+{ id: 3, name: 'Produto 3', image: MensWoolCruiser, price: 85, quantity: 4 },
+{ id: 4, name: 'Produto 4', image: MensWoolCruiserSlipOn, price: 135, quantity: 6 },
+{ id: 5, name: 'Produto 5', image: MensWoolCruiserWaterproof, price: 15, quantity: 2 }
+]
+
+export const ShoppingCart = () => {
+
+    const [cartIsOpen, setCartIsOpen] = useState<boolean>(false)
+
+    return (
+        <>
+            <button className="cursor-pointer" onClick={() => setCartIsOpen(!cartIsOpen)}>
+                <img src={Iconcart} alt="Icone carrinho de compras" />
+            </button>
+            {/* Overlay */}
+            <div
+                className={`${cartIsOpen ? "w-full bg-black/60" : "w-0"} fixed top-0 bottom-0 left-0`} onClick={() => setCartIsOpen(!cartIsOpen)}>
+
+                {/* Drawer */}
+                <div className={`${cartIsOpen ? "w-75 md:w-106" : "w-0"} absolute top-0 right-0 bottom-0 bg-white pt-6 transition-all duration-500 ease-in-out`} onClick={(e) => e.stopPropagation()}>
+                    <header className="flex items-center justify-between px-5">
+                        <p className="text-2xl font-bold">
+                            Carrinho
+                        </p>
+                        <button className="text-xl cursor-pointer">
+                            X
+                        </button>
+                    </header>
+
+                    <ul className="p-4 h-[calc(100%-130px)] overflow-y-auto scrollbar-hide flex flex-col gap-3">
+                        {productsInCart.map(product => (
+                            <li key={product.id} className="flex flex-col gap-1 px-6">
+                                <button className="self-end text-xs cursor-pointer">X</button>
+
+                                <div className="flex gap-4">
+                                    <img src={product.image} alt={product.name} className="w-16 h-16" />
+
+                                    <div className="flex flex-col items-start">
+                                        <p className="mb-1 text-sm">{product.name}</p>
+                                        <p className="mb-1 text-sm">Quantidade: {product.quantity}</p>
+                                        <p className="mb-3.5">
+                                            <span className="font-semibold mr-1.5">
+                                                {formatCurrency(product.price)} {" "} á vista
+                                            </span>
+                                        </p>
+
+                                        <div className="border flex gap-6 py-2 px-3">
+                                            <button className="cursor-pointer">-</button>
+                                            <p>{product.quantity}</p>
+                                            <button className="cursor-pointer">+</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <footer className="abdolute bottom-0 p-4 w-full h-[100px]">
+                        <button className="w-full h-full bg-black text-white rounded-xs cursor-pointer hover:bg-gray-800">Fechar Pedido</button>
+                    </footer>
+
+                </div>
+            </div>
+        </>
+    )
+}
